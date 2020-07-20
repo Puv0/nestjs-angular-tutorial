e712f5d07ec96f8cef5a81094eb7bf26a5af5157
Puv0 <59066014+Puv0@users.noreply.github.com>
2020-07-20 13:55:14 +0300

    Update enviroment.ts

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,5 +1,5 @@
 export default {
 
-    mongoUrl: 'mongodb+srv://puv0:19986974@cluster0.rlhsg.mongodb.net/Cluster0?retryWrites=true&w=majority'
+    mongoUrl: 'mongodb+srv://@.rlhsg.mongodb.net/?retryWrites=true&w=majority'
     
 }
